bc10c5c67acfc3b3435b361ad9c664454f887708
crystal <crystalevenram@gmail.com>
2024-06-04 22:18:00 -0500

    completed input form, button and .ma p rendering the list and the value of the input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,55 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+
 
 function App() {
 
+    // * Local state for the airlines. (To hold the airline before we send in our dispatch with handleSubmit)
+
+  const [airline,setAirline] = useState('')
+
+    // * Getting the value, useSelector hook
+const planeList = useSelector(store => store.planeList)
+
+// * useDispatch to set the value of state, needs to be initialized
+  // Dispatch, will communicate with all reducers.
+  // Actions distinguish wich reducers should react.
+
+  const dispatch = useDispatch()
+
+  let handleSubmit = (event) => { 
+    event.preventDefault()
+    console.log("handleSubmit() is activated! ... adding...", airline)
+  
+
+  // will make a dispatch to add a new airline
+
+  dispatch({
+    type: "ADD_AIRLINE",
+    payload: airline
+  })
+
+  // Need to clear the form
+    setAirline('')
+}
+
   return (
+    <>
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <form onSubmit={handleSubmit}>
+      <input placeholder='Airline Name' 
+      value={airline}
+      onChange={event => setAirline(event.target.value)}/>
+      <button type='submit' >Add Airline</button>
+       </form>
+      <table>{/* Airlines should be listed here */}
+      {planeList.map(item => <li key={item}>{item}</li>)}
+
+      </table>
+     
     </div>
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,32 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
 /** TODO: import REDUX **/
-
+import { applyMiddleware, createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
 /** TODO: Add REDUCERS */
-
+const planeList = (state = ["American"], action) => {
+    console.log("airlineList reduce activated!", action)
+    if (action.type === "ADD_AIRLINE"){
+    //console.log(`The airline was ${action.payload}`);
+    return [...state, action.payload];
+}
+return state
+}
 
 /** TODO: Create store */
-
-
+const storeInstance = createStore(
+    combineReducers({
+        planeList,
+      })
+)
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+         <Provider store={storeInstance}>
+      <App />
+    </Provider>
     </React.StrictMode>
 );
